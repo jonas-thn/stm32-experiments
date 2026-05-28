584ec4b70849d479d709d6500e54b25b99386229
Jonas Thaun <thaunjonas@gmail.com>
2026-05-28 23:40:14 +0200

    cmake workflow

---
+++ CMake-Test/build/CMakeFiles/3.31.2/CMakeASMCompiler.cmake
@@ -0,0 +1,28 @@
+set(CMAKE_ASM_COMPILER "C:/ST/STM32CubeIDE_2.1.1/STM32CubeIDE/plugins/com.st.stm32cube.ide.mcu.externaltools.gnu-tools-for-stm32.14.3.rel1.win32_1.0.100.202602081740/tools/bin/arm-none-eabi-gcc.exe")
+set(CMAKE_ASM_COMPILER_ARG1 "")
+set(CMAKE_AR "C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.44.35207/bin/Hostx64/x64/lib.exe")
+set(CMAKE_ASM_COMPILER_AR "C:/ST/STM32CubeIDE_2.1.1/STM32CubeIDE/plugins/com.st.stm32cube.ide.mcu.externaltools.gnu-tools-for-stm32.14.3.rel1.win32_1.0.100.202602081740/tools/bin/arm-none-eabi-gcc-ar.exe")
+set(CMAKE_RANLIB ":")
+set(CMAKE_ASM_COMPILER_RANLIB "C:/ST/STM32CubeIDE_2.1.1/STM32CubeIDE/plugins/com.st.stm32cube.ide.mcu.externaltools.gnu-tools-for-stm32.14.3.rel1.win32_1.0.100.202602081740/tools/bin/arm-none-eabi-gcc-ranlib.exe")
+set(CMAKE_LINKER "C:/ST/STM32CubeIDE_2.1.1/STM32CubeIDE/plugins/com.st.stm32cube.ide.mcu.externaltools.gnu-tools-for-stm32.14.3.rel1.win32_1.0.100.202602081740/tools/bin/arm-none-eabi-g++.exe")
+set(CMAKE_LINKER_LINK "C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.44.35207/bin/Hostx64/x64/link.exe")
+set(CMAKE_LINKER_LLD "lld-link")
+set(CMAKE_ASM_COMPILER_LINKER "")
+set(CMAKE_ASM_COMPILER_LINKER_ID "")
+set(CMAKE_ASM_COMPILER_LINKER_VERSION )
+set(CMAKE_ASM_COMPILER_LINKER_FRONTEND_VARIANT )
+set(CMAKE_MT "CMAKE_MT-NOTFOUND")
+set(CMAKE_TAPI "")
+set(CMAKE_ASM_COMPILER_LOADED 1)
+set(CMAKE_ASM_COMPILER_ID "GNU")
+set(CMAKE_ASM_COMPILER_VERSION "")
+set(CMAKE_ASM_COMPILER_ENV_VAR "ASM")
+
+
+
+
+set(CMAKE_ASM_IGNORE_EXTENSIONS h;H;o;O;obj;OBJ;def;DEF;rc;RC)
+set(CMAKE_ASM_LINKER_PREFERENCE 0)
+set(CMAKE_ASM_LINKER_DEPFILE_SUPPORTED )
+
+
